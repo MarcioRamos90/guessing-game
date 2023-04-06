7194d639d0230c3301c304910d00486cde09c768
Marcio Ramos <marcioalvesramos90@hotmail.com>
2023-04-06 08:48:17 -0300

    generate random number

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+#define rand_num(min, max) \
+    ((rand() % (int)(((max) + 1) - (min))) + (min))
+
 
 int get_number_input() {
     int input;
@@ -12,5 +19,14 @@ int main() {
     // get input number
     int guessing = get_number_input();
 
-    printf("your try is: %d", guessing);
+    printf("\nyour try is: %d", guessing);
+    
+    // generate random number
+    long int seed = time(NULL) % clock();
+    
+    srand(seed);
+    
+    int secret_number = rand_num(70, 100);
+
+    printf("\nThe secret: %d", secret_number);
 }
